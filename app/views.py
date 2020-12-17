6bcda54f9994c0c6f3972242a45fd28bacc51e70
rykovskov.m <rykovskov.m@as61.ru>
2020-12-17 22:34:38 +0300

    relize6

--- app/views.py
+++ app/views.py
@@ -172,8 +172,8 @@ def vpn_users():
                         al_ip = al_ip+ip_adr.ip_allowedips+'/'+ip_adr.mask_allowedips + ','
                 else:
                     print('res_ip1111 ', res_ip[0])
-                    al_ip = res_ip[0]
-                conf.append('AllowedIPs = ' + str(al_ip) + '\n')
+                    al_ip = res_ip[0][0]+'/' + res_ip[0][1]
+                conf.append('AllowedIPs = ' + al_ip + '\n')
                 conf.append('Endpoint =  ' + res_server.server_organizations + ':' + str(res_server.port) + '\n')
                 conf.append('PersistentKeepalive = 25\n')
                 print(conf)
