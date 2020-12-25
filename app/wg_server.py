939ca63426e0ca61059d9d27f444edffd44246d5
rykovskov.m <rykovskov.m@as61.ru>
2020-12-25 09:21:59 +0300

    iptables

--- app/wg_server.py
+++ app/wg_server.py
@@ -60,7 +60,7 @@ if res[0][0]:
             cur.execute(sql_select_allowips,(vpn_user[0],))
             allow_ips = cur.fetchall()
             for allow_ip in allow_ips:
-                ipt.append('iptables -A FORWARD -s ' + vpn_user[1] + ' -d ' + allow_ip[0]+' -j LOG --log-prefix "peer ' + vpn_user[2] + '"\n')
+                ipt.append('iptables -A FORWARD -s ' + vpn_user[1] + ' -d ' + allow_ip[0]+' -j LOG --log-prefix "peer ' + vpn_user[3] + '"\n')
 
         with codecs.open(name_wg_interface_new_file, 'w', encoding='UTF8') as f:
             for item in conf:
