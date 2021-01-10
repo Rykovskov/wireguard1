94f069e4568c74478fde02d3d95cc376dea141b2
rykovskov.m <rykovskov.m@as61.ru>
2021-01-10 12:25:08 +0300

    Add rebuld_config for org

--- app/wg_server.py
+++ app/wg_server.py
@@ -8,12 +8,16 @@ import codecs
 import socket
 
 hostname = socket.gethostname()
+print('name host - ', hostname)
 wireguard_patch = '/etc/wireguard'
 prefix_wg_config = 'wg_'
 ip_tables_name_file = '/etc/wireguard/iptables.sh'
 conn = psycopg2.connect(dbname='WireGuardUsers', user='flask', password='freud105b', host='localhost')
 sql_select_rebuild = """select * from rebuild_config  order by last_update desc limit 1"""
-sql_select_org = """select id_organizations, name_organizations, server_organizations, public_vpn_key_organizations, private_vpn_key_organizations, port, subnet, replace(subnet,'.1/', '.0/') as subnet_1 from organizations where  id_organizations = %s"""
+sql_select_org = """select id_organizations, name_organizations, server_organizations, public_vpn_key_organizations, 
+                    private_vpn_key_organizations, port, subnet, 
+                    replace(subnet,'.1/', '.0/') as subnet_1 from organizations 
+                    where    (id_organizations = %s) and (select rebuld from public.rebuild_config where org =%s)"""
 sql_select_users = """select  id_vpn_users, adres_vpn, (select publickey from vpn_key where id_vpn_key=vpn_users.vpn_key) as p_key, name_vpn_users from vpn_users where active_vpn_users=true and organizations =  %s"""
 sql_select_allowips = """select ip_allowedips||'/'||mask_allowedips from public.allowedips where vpn_user= %s"""
 sql_update_rebuild = """update rebuild_config set rebuld=false"""
