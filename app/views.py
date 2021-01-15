c4768b66ae50a530d9ae81e98c10e952d7604a87
rykovskov.m <rykovskov.m@as61.ru>
2021-01-15 11:28:15 +0300

    edit vpn users

--- app/views.py
+++ app/views.py
@@ -282,10 +282,11 @@ def edit_vpn_users():
     user = Vpn_users.query.get(id_user)
     #Получаем список разрешенных ип
     res_ip = Allowedips.query.filter_by(vpn_user=id_user).all()
-    print('res_ip', res_ip)
-    form = EditVpnUserForm(vpn_login=user.name_vpn_users, email_vpn_users=user.email_vpn_users, adres_vpn=user.adres_vpn, allowedips_ip=res_ip)
+    sp_ip =[]
+    for ip in res_ip:
+        sp_ip.append(ip)
+    form = EditVpnUserForm(vpn_login=user.name_vpn_users, email_vpn_users=user.email_vpn_users, adres_vpn=user.adres_vpn, allowedips_ip=sp_ip)
     form.edit_vpn_organizations.choices = res_org
-    print(user)
     if request.method == 'POST':
         result = request.form
     return render_template('edit_vpn_user.html', form=form, cur_user=current_user.name_users)
