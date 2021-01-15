b873ef70c1a770827dbb43ca5dec560f2f130e82
rykovskov.m <rykovskov.m@as61.ru>
2021-01-15 21:25:53 +0300

    edit vpn users

--- app/views.py
+++ app/views.py
@@ -285,13 +285,13 @@ def edit_vpn_users():
     s = ''
     for ip in res_ip:
         s = s + str(ip) + '\n'
-    form = EditVpnUserForm(vpn_login=user.name_vpn_users, email_vpn_users=user.email_vpn_users, adres_vpn=user.adres_vpn, allowedips_ip=s[:-1:])
+    form = EditVpnUserForm() #vpn_login=user.name_vpn_users, email_vpn_users=user.email_vpn_users, adres_vpn=user.adres_vpn, allowedips_ip=s[:-1:])
     form.edit_vpn_organizations.choices = [(row.id_organizations, row) for row in Organizations.query.all()]
     form.edit_vpn_organizations.default = user.organizations
-    #form.vpn_login = user.name_vpn_users
-    form.email_vpn_users = user.email_vpn_users
-    #form.adres_vpn = user.adres_vpn
-    #form.allowedips_ip = s[:-1:]
+    form.vpn_login.data = user.name_vpn_users
+    form.email_vpn_users.data = user.email_vpn_users
+    form.adres_vpn.data = user.adres_vpn
+    form.allowedips_ip.data = s[:-1:]
     form.process()
     if request.method == 'POST':
         result = request.form
