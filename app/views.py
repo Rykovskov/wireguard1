2336a2220dc00d70af961531d9da4d37e5aa515d
rykovskov.m <rykovskov.m@as61.ru>
2021-01-15 19:43:40 +0300

    edit vpn users

--- app/views.py
+++ app/views.py
@@ -287,7 +287,7 @@ def edit_vpn_users():
         s = s + str(ip) + '\n'
     form = EditVpnUserForm(vpn_login=user.name_vpn_users, email_vpn_users=user.email_vpn_users, adres_vpn=user.adres_vpn, allowedips_ip=s[:-1:])
     form.edit_vpn_organizations.choices = res_org
-    form.edit_vpn_organizations.default=3
+    form.edit_vpn_organizations.data = 3
     if request.method == 'POST':
         result = request.form
     return render_template('edit_vpn_user.html', form=form, cur_user=current_user.name_users)
