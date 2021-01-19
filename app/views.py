4dd6e4fbba7e2eadd3e962e50ad4e0b82ef41b45
rykovskov.m <rykovskov.m@as61.ru>
2021-01-19 11:22:23 +0300

    Add validate IP adress

--- app/views.py
+++ app/views.py
@@ -165,7 +165,7 @@ def vpn_users():
 
     if request.method == 'GET':
         print('----------------GET-------------------')
-        res = Vpn_users.query.filter_by(active_vpn_users='True', organizations=4).order_by(Vpn_users.name_vpn_users).all()
+        res = Vpn_users.query.filter_by(active_vpn_users='True', organizations=3).order_by(Vpn_users.name_vpn_users).all()
         form.v_user.data = False
         return render_template('vpn_user.html', form=form, cur_user=current_user.name_users, sp_vpn_users=res)
 
